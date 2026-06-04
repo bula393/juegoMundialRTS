979cbe4b61beabeb88c818922c0adbe3490ca6da
bula393 <alumno26.buhler.santiago@ipm.edu.ar>
2026-06-04 20:41:56 -0300

    creacion de paises jugables y sorteo de resto de territorios hecho

--- rts-mundial/scripts/mapa.gd
+++ rts-mundial/scripts/mapa.gd
@@ -1,14 +1,24 @@
 extends Node2D
+
+enum paises {ARGENTINA, BRASIL, CHILE, MEXICO, USA, CANADA, EGIPTO, ETIOPIA, SAHARA, SUDAFRICA,ALEMANIA,FRANCIA,INGLATERRA,AUSTRALIA,SANMARINO,RUSIA,CHINA, INDIA}
+
+var paisesSeleccionados = ["ARGENTINA","MEXICO","SUDAFRICA"]
+
+var paisesAsortiar = []
+
+
+	
+
+
 @onready var menu_pais = $"../menuPais"
 var paisesJugable:Array[PaisJugable] = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	for pais in get_children():
-		var p : PaisJugable = PaisJugable.new()
-		p.crear(pais)
-		p.menu_pais = menu_pais
-		paisesJugable.append(p)
+	armarPaisesJugables()
+	sortiarPaisesRestantes()
+		
+		
 
 	
 
@@ -16,3 +26,27 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func armarPaisesJugables() -> void:
+	for pais in get_children():
+		var estaSelecciona = false
+		for p in paisesSeleccionados:
+			if pais.name.to_upper() == p:
+				estaSelecciona =true
+		if estaSelecciona:		
+			var p : PaisJugable = PaisJugable.new()
+			p.crear(pais)
+			p.menu_pais = menu_pais
+			paisesJugable.append(p)
+		else:
+			paisesAsortiar.append(pais)
+	
+func sortiarPaisesRestantes() -> void:
+	var cantidadPorPais = paisesAsortiar.size() / paisesJugable.size()
+	
+	for pais in paisesJugable:
+		for i in range(cantidadPorPais):
+			var paisAgregado = paisesAsortiar.pick_random()
+			pais.agregaTerritorio(paisAgregado)
+			paisesAsortiar.erase(paisAgregado)
